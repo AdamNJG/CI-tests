7483372071be1d0ddfa2c8240ab52ce5d8090757
adam goldsmith <adam.goldsmith0@gmail.com>
2023-02-08 11:28:19 +0000

    Revert "Revert "failing again""
    This reverts commit 631b55f76c97ffabb77b96d2133454fb7de11585.

---
+++ src/tests/userTests.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from '@jest/globals';
+import User from '../user';
+
+describe('User Tests', () =>{
+
+  test('User first name, last name and age match input', () => {
+    let firstName = "bob";
+    let lastName = "smith";
+
+    var user = new User(firstName, lastName);
+    expect(user.firstName).toBe(firstName);
+    expect(user.lastName).toBe(lastName);
+  });
+
+  fail
+});
