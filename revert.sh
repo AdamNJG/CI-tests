0f89855a71a5e5468b0f1ed31fabf4aecf9962fe
adam goldsmith <adam.goldsmith0@gmail.com>
2023-02-08 11:43:46 +0000

    Revert "Revert "added git config stuff""
    This reverts commit 34d1b04b2fb8a51f12eea7f45d4a5bfd7310d68f.

---
+++ revert.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+if [ -z "$1" ]
+then 
+  echo input is empty! 
+  exit -1
+fi
+
+echo $1
+# get the SHA to revert
+COMMIT_TO_REVERT=$1
+
+REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
+echo "Collecting information about PR #$PR_NUMBER of $REPO_FULLNAME..."
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+	echo "Set the GITHUB_TOKEN env variable."
+	exit 1
+fi
+
+echo $REPO_FULLNAME
+
+HEAD_BRANCH="main"
+
+git config --global user.email "AdamNJG@github.com"
+git config --global user.name "ANJG auto revert"
+
+git revert $COMMIT_TO_REVERT --no-edit
+git push origin $HEAD_BRANCH
